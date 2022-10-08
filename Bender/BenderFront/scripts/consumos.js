742d6f2080da7c2e2a3f6b6357a8e9a8240a8895
juanforeroid <86366424+juanforeroid@users.noreply.github.com>
2022-10-07 23:27:16 -0500

    Front v2 Metodos Usuarios

--- Bender/BenderFront/scripts/consumos.js
+++ Bender/BenderFront/scripts/consumos.js
@@ -19,7 +19,19 @@ function ajaxlogin(userName, password) {
         "user": userName,
         "password": password
     }
-    return consumoAjax("POST", url, JSON.stringify(body), "application/json;charset=UTF-8");
+    return consumoAjax("POST", url, JSON.stringify(body), "aplication/json");
+}
+//---------------------------------------------------------------------------------------
+function insertUser(identification, name, password, rolId, idbranch) {
+    url = urlBase + "User/Insert";
+    body = {
+        "identification": identification,
+        "name": name,
+        "password": password,
+        "rolId": rolId,
+        "idbranch": idbranch
+    }
+    return consumoAjax("POST", url, JSON.stringify(body), "aplication/json");
 }
 //---------------------------------------------------------------------------------------
 function deleteUser(userId) {
@@ -27,13 +39,13 @@ function deleteUser(userId) {
 }
 //---------------------------------------------------------------------------------------
 function rolsGetAll() {
-    return consumoAjax("GET", urlBase + "Roles/GetAll", "", "");
-    //return consumoAjax("GET", "consumos/listroles.json", "", "");
+    //return consumoAjax("GET", urlBase + "Roles/GetAll", "", "");
+    return consumoAjax("GET", "consumos/listroles.json", "", "");
 }
 //---------------------------------------------------------------------------------------
 function userGetAll() {
-    return consumoAjax("GET", urlBase + "User/GetAll", "", "");
-    //return consumoAjax("GET", "consumos/listuser.json", "", "");
+    //return consumoAjax("GET", urlBase + "User/GetAll", "", "");
+    return consumoAjax("GET", "consumos/listuser.json", "", "");
 }
 //---------------------------------------------------------------------------------------
 function consumoAjax(method, url, body, contentType) {
