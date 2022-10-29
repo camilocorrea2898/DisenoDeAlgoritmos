8e6b7be81cdbe83d848f218a2cd5854e81208f17
juanforeroid <86366424+juanforeroid@users.noreply.github.com>
2022-10-29 10:46:23 -0500

    Update Roles

--- Bender/BenderFront/scripts/rols.js
+++ Bender/BenderFront/scripts/rols.js
@@ -6,13 +6,18 @@ function validarRol() {
     }
     else {
 
-        if (rolActual == 1001) {
+        if (rolActual == 1) {
             $("#admon").show();
+            $("#sales").show();
             $("#reports").show();
+        }
+        else if( rolActual == 2 ){
+            $("#sales").show();
         }
         else {
             $("#admon").hide();
             $("#reports").hide();
+            $("#sales").hide();
         }
 
         cargar("index");
